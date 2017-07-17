1b8a81aa2729a3cc8c7ea8b07259ae52eb36f47a
nucasspro local <hailongdang.hailong@gmail.com>
2017-07-18 01:32:18 +0700

    Complete Turn On and Turn Off SQL Service

--- Remote SQL Services/Remote SQL Services/MainWindow.xaml.cs
+++ Remote SQL Services/Remote SQL Services/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ServiceProcess;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Remote_SQL_Services
 {
@@ -20,9 +10,87 @@ namespace Remote_SQL_Services
     /// </summary>
     public partial class MainWindow : Window
     {
+        public object ServicesController { get; private set; }
+        public List<string> list = new List<string>();
+        public NotifyIcon notifi = new NotifyIcon();
+
         public MainWindow()
         {
             InitializeComponent();
+            Load();
         }
+
+        #region Event
+
+        private void btnTurnOn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in list)
+            {
+                TurnOn(item);
+            }
+            Notification("All SQL Services has been started!");
+        }
+
+        private void btn_TurnOff_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in list)
+            {
+                TurnOff(item);
+            }
+            Notification("All SQL Services has been stopped!");
+        }
+
+        #endregion Event
+
+        #region Method
+
+        private void Load()
+        {
+            list.Add("MSSQLFDLauncher");
+            list.Add("MSSQLSERVER");
+            list.Add("SQLSERVERAGENT");
+            list.Add("MSSQLServerOLAPService");
+            list.Add("SSASTELEMETRY");
+            list.Add("SQLTELEMETRY");
+            list.Add("SQL Server Distributed Replay Client");
+            list.Add("SQL Server Distributed Replay Controller");
+            list.Add("MsDtsServer130");
+            list.Add("SSISTELEMETRY130");
+            list.Add("MSSQLLaunchpad");
+            list.Add("ReportServer");
+            list.Add("SQLWriter");
+            notifi.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetEntryAssembly().ManifestModule.Name);
+            notifi.Visible = true;
+        }
+
+        private void TurnOff(string service_name)
+        {
+            ServiceController services = new ServiceController(service_name);
+            if (services.Status == ServiceControllerStatus.Stopped)
+            {
+                return;
+            }
+            services.Stop();
+            services.WaitForStatus(ServiceControllerStatus.Stopped);
+        }
+
+        private void TurnOn(string service_name)
+        {
+            ServiceController services = new ServiceController(service_name);
+            if (services.Status == ServiceControllerStatus.Running)
+            {
+                return;
+            }
+            services.Start();
+            services.WaitForStatus(ServiceControllerStatus.Running);
+        }
+
+        private void Notification(string message)
+        {
+            notifi.BalloonTipText = message;
+            notifi.ShowBalloonTip(1000);
+        }
+
+        #endregion Method
     }
 }
--- Remote SQL Services/Remote SQL Services/Properties/AssemblyInfo.cs
+++ Remote SQL Services/Remote SQL Services/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -8,7 +7,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Remote SQL Services")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Made by Nucasspro")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Remote SQL Services")]
@@ -30,7 +29,6 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
@@ -39,7 +37,6 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
 
 // Version information for an assembly consists of the following four values:
 //
@@ -53,3 +50,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
